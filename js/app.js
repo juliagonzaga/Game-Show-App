8029d2ccc3bfb9c3329831ec61d6def848887d12
Julia <shaow.1230@gmail.com>
2020-08-23 03:03:04 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const key = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const start = document.querySelector('.btn__reset');
 const qwerty = document.querySelector('#qwerty');
+const scoreboard = document.querySelector('#scoreboard ol');
+const win = document.querySelector('.win');
+let lives = 5;
 let missed = 0;
 
 let phrases = [ 
@@ -36,11 +39,15 @@ function  addPhraseToDisplay(arr) {
     }
 };
 
+addPhraseToDisplay(getRandomPhraseAsArray(phrases));
+let letter = document.querySelectorAll('.letter');
+
 function checkLetter(clickedBtn) {
-    let letter = document.querySelectorAll('.letter');
     let correctGuess = null;
+
     for (let i = 0; i < letter.length; i++ ){
         let letterGuess = letter[i].textContent;
+
         if (letterGuess === clickedBtn) {
             letter[i].className = 'show';
             correctGuess = letterGuess;
@@ -49,20 +56,51 @@ function checkLetter(clickedBtn) {
     return correctGuess;
 };
 
+function updateLives() {
+    scoreboard.innerHTML = '';
+
+    for(let i = 0; i < missed; i++){
+        let list = document.createElement('li');
+            list.innerHTML = `<img src="images/lostHeart.png" height="35px" width="30px">`;
+            scoreboard.appendChild(list);
+    }
+
+    for(let i = 0; i < lives; i++){
+        let list = document.createElement('li');
+            list.innerHTML = `<img src="images/liveHeart.png" height="35px" width="30px">`;
+            list.className = 'tries';
+            scoreboard.appendChild(list);
+    }
+};
+
+function checkWin() {
+    let correctLetter = document.querySelectorAll('.show');
+        correctLetter = correctLetter.length;
+    let totalLetters = letter.length;
+
+    if(correctLetter === totalLetters) {
+        console.log('You Win');
+    };
+};
+
 qwerty.addEventListener('click', (e) => {
     e.preventDefault();
     let target = e.target;
+    
     if (target.tagName == 'BUTTON') {
         target.className = 'chosen';
         target.setAttribute('disabled', '');
-        let guessed = target.textContent;
-        let letterFound = checkLetter(guessed);
+        let guess = target.textContent;
+        let letterFound = checkLetter(guess);
         
         if (letterFound === null) {
+            lives--;
             missed++;
+
+            updateLives();
         }
     }
+    checkWin()
 });
 
-let phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
+updateLives();
